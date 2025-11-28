4e524c5056cffc0ec68a1730d838daf67f7c26f4
GetApPRO <81932927+getappro@users.noreply.github.com>
2025-11-28 17:42:34 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
+import { useCart } from '../context/CartContext'
 
 export default function Header() {
+  const { totalItems } = useCart()
+
   return (
     <header className="header">
       <div className="logo">
@@ -8,7 +11,7 @@ export default function Header() {
       </div>
       <nav>
         <Link href="/">Accueil</Link>
-        <Link href="/cart">Panier</Link>
+        <Link href="/cart">Panier ({totalItems})</Link>
       </nav>
     </header>
   )
